6799c2906021b7619255e3995a235a28239ca584
P12_Draco <draco@p12.dev>
2024-01-02 14:09:41 +0800

    🪜Script deployment example

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,4 @@
 import hre from "hardhat";
-import { ethers } from "hardhat";
 
 // Colour codes for terminal prints
 const RESET = "\x1b[0m";
@@ -10,10 +9,10 @@ function delay(ms: number) {
 }
 
 async function main() {
-  const [owner, account] = await ethers.getSigners();
+  //const [owner, account] = await ethers.getSigners();
 
-  const constructorArgs = ["tokenUri", owner.address, account.address];
-  const contract = await hre.ethers.deployContract("/contracts/src/modragon/MoDragonContract.sol:MoDragonContract", constructorArgs);
+  const constructorArgs = ["tokenUri", "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"];
+  const contract = await hre.ethers.deployContract("/contracts/src/drawnBringer/DrawnBringerNFT.sol:DrawnBringerNFT", constructorArgs);
 
   await contract.waitForDeployment();
   const contractAddress = await contract.getAddress();
@@ -21,15 +20,15 @@ async function main() {
   console.log("Greeter deployed to: " + `${GREEN}${contractAddress}${RESET}\n`);
 
   // TODO Uncomment if you want to enable the `verify`
-  // console.log(
-  //   "Waiting 30 seconds before beginning the contract verification to allow the block explorer to index the contract...\n",
-  // );
-  // await delay(30000); // Wait for 30 seconds before verifying the contract
+  console.log(
+    "Waiting 30 seconds before beginning the contract verification to allow the block explorer to index the contract...\n",
+  );
+  await delay(30000); // Wait for 30 seconds before verifying the contract
 
-  // await hre.run("verify:verify", {
-  //   address: contractAddress,
-  //   constructorArguments: constructorArgs,
-  // });
+  await hre.run("verify:verify", {
+    address: contractAddress,
+    constructorArguments: constructorArgs,
+  });
 
   // TODO Uncomment if you want to enable the `tenderly` extension
   // await hre.tenderly.verify({
