c748d78bc00768a2670f7b247c28240b69ef742f
P12_Draco <draco@p12.dev>
2024-01-29 17:24:34 +0800

    👷Improve the interface contract

--- contracts/src/zetaChain/interfaces/IP12xZetachainOmniBadge.sol
+++ contracts/src/zetaChain/interfaces/IP12xZetachainOmniBadge.sol
@@ -2,14 +2,35 @@
 pragma solidity >=0.8.7;
 
 interface IP12xZetachainOmniBadgeDef {
-    error MintExpired();
-    error AlreadyReceived();
-    error InvalidSignature();
+    // =========== ERROR ==========
+    // error MintExpired();
+    error NonSystemContract();
 
+    error AlreadyReceivedNFT();
+    error AlreadyReceivedCost();
+
+    // error InvalidSignature();
+    error ZETATransferFailed();
+    error IncorrectAmountZETA();
+    error InsufficientZETABalance();
+
+    error votingHasEnded();
+    error UserVotedThatDay();
+    error MintBehaviorNotChecked();
+    error ReceiveCostTimeNotArrived();
+
+    // =========== EVENT ==========
     event SetSigner(address newSigner);
     event SetTokenUri(string newTokenURI);
+    event SetPayQuantity(uint256 newPayQuantity);
     event SetSystemContract(address newSystemContract);
-    event UserHasReceived(uint256 indexed tokenID, address indexed userAddress);
+    event SetVoteStartTimeAndDurationDays(uint256 newVoteStartTimeAndDurationDays);
+
+    event CollectionStartsStatus(bool indexed isOpen);
+
+    event UserHasReceivedCost(address indexed userAddress);
+    event UserHasReceivedNFT(uint256 indexed tokenID, address indexed userAddress);
+    event UserVoteForGameID(address indexed userAddress, uint256 indexed timeStamp, uint256 indexed gameID);
 }
 
 interface IP12xZetachainOmniBadge is IP12xZetachainOmniBadgeDef {}
