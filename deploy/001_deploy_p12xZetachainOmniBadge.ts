c03e4a20b40819c4da9d513d1d69551db95bd039
P12_Draco <draco@p12.dev>
2024-01-29 22:10:36 +0800

    🩻Zeta chain contract deploy

--- deploy/001_deploy_p12xZetachainOmniBadge.ts
+++ deploy/001_deploy_p12xZetachainOmniBadge.ts
@@ -1,48 +1,73 @@
-import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
-import { keccak256, stringToBytes, encodeFunctionData } from 'viem';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { keccak256, stringToBytes, encodeFunctionData } from "viem";
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function storeData(
+  votingStartTime: number,
+  votingDurationDays: number,
+): number {
+  // 将votingDurationDays放在最低的64位
+  const durationData: number = votingDurationDays;
+
+  // 将votingStartTime左移64位，为votingDurationDays留出空间
+  const startTimeData: number = votingStartTime << 64;
+
+  // 通过按位或操作(|)将两个值合并到一个uint256中
+  return startTimeData | durationData;
 }
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
 
-    /**
-     * system contract,
-     * token uri,
-     * signer Address
-     */
+  // 2024-01-30 00:00:01
+  // 99 days
+  let voteStartTimeAndDurationDays = storeData(1706544001, 99);
+
+  console.log(deployer)
+  /**
+   *  _voteStartTimeAndDurationDays,
+   *  _systemContractAddress,
+   *  _tokenUri,
+   *  _initialOwner,
+   *  _payQuantity
+   */
   const args = [
-    "0x0000000000000000000000000000000000000000",
+    voteStartTimeAndDurationDays,
+    "0x803470638940Ec595B40397cbAa597439DE55907",
     "test token uri",
-    "0x803470638940Ec595B40397cbAa597439DE55907"
+    deployer,
+    "100000000000000000" // 0.1ETH
   ];
 
-  let contract = await deploy('P12xZetachainOmniBadge', {
+  let contract = await deploy("P12xZetachainOmniBadge", {
     from: deployer,
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     proxy: {
       // 取消ProxyAdmin检查
-      checkProxyAdmin:false,
-      proxyContract: 'ERC1967Proxy',
-      proxyArgs: ['{implementation}', '{data}'],
+      checkProxyAdmin: false,
+      proxyContract: "ERC1967Proxy",
+      proxyArgs: ["{implementation}", "{data}"],
       // 更新合约需要被调用的方法
       upgradeFunction: {
         methodName: "upgradeToAndCall",
-        upgradeArgs: ['{implementation}', '{data}']
+        upgradeArgs: ["{implementation}", "{data}"],
       },
       execute: {
         init: {
-          methodName: 'initialize',
+          methodName: "initialize",
           args: args,
         },
       },
     },
-    deterministicDeployment: keccak256(stringToBytes('P12xZetachainOmniBadge_PROD')),
+    deterministicDeployment: keccak256(
+      stringToBytes("P12xZetachainOmniBadge_PROD"),
+    ),
   });
 
   // TODO If you want to cancel "verification", please enable comments
@@ -62,7 +87,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   //   address: contract.address,
   //   constructorArguments: [contract.address, data],
   // });
-}
+};
 export default func;
-func.id = '001_deploy_p12xZetachainOmniBadge'; // id required to prevent reexecution
-func.tags = ['P12xZetachainOmniBadge'];
+func.id = "001_deploy_p12xZetachainOmniBadge"; // id required to prevent reexecution
+func.tags = ["P12xZetachainOmniBadge"];
