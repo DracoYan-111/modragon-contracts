c2a24249cc423baeed4eb06f8057855b6aa0154b
P12_Draco <draco@p12.dev>
2024-01-29 17:25:43 +0800

    📄Modify according to the document

--- contracts/src/zetaChain/P12xZetachainOmniBadge.sol
+++ contracts/src/zetaChain/P12xZetachainOmniBadge.sol
@@ -32,24 +32,53 @@ contract P12xZetachainOmniBadge is
         0x1cd131e6d0ca7e68ef19f43129322449a3aeef57e2f4bd8760e571742140ec00;
 
     struct P12xZetachainOmniBadgeStorage {
-        address _signer;
         string _tokenURI;
-        uint256 _nextTokenId;
+        uint64 payQuantity;
+        uint128 _nextTokenId;
+        bool collectionStarts;
+        BitMaps.BitMap userReceiveNFT;
+        BitMaps.BitMap userReceiveCost;
+        uint256 voteStartTimeAndDurationDays;
+        mapping(address => uint256) userPaysFees;
+        mapping(address => mapping(uint256 => uint256)) userVotingStatus;
         SystemContract systemContract;
-        BitMaps.BitMap userReceive;
     }
 
-    modifier onlyCanMintOnce() {
+    modifier onlyCanMintOnceAndVerifyPayment() {
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
-        if ($.userReceive.get(uint256(uint160(msg.sender)))) revert AlreadyReceived();
+        if (msg.value != $.payQuantity) revert IncorrectAmountZETA();
+        if ($.userReceiveNFT.get(uint256(uint160(msg.sender)))) revert AlreadyReceivedNFT();
+
+        _;
+    }
+
+    modifier onlyCanOpenAndReceivedOnce() {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        if (!$.collectionStarts) revert ReceiveCostTimeNotArrived();
+        if (!BitMaps.get($.userReceiveCost, uint256(uint160(msg.sender))) && $.userPaysFees[msg.sender] == 0)
+            revert AlreadyReceivedCost();
+
+        _;
+    }
+
+    modifier onlyDaysUpdateAndMintBehavior(){
+
+        uint256 voteDays = getVoteDays();
+
+        if (voteDays == getVotingDurationDays()) revert votingHasEnded();
+        if (!getUserReceiveNFT(msg.sender)) revert MintBehaviorNotChecked();
+        if (getUserVoteEligibility(msg.sender, voteDays)) revert UserVotedThatDay();
+
         _;
     }
 
     modifier onlySystem() {
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
-        require(msg.sender == address($.systemContract), "Only system contract can call this function");
+        if (msg.sender != address($.systemContract)) revert NonSystemContract();
+
         _;
     }
 
@@ -58,20 +87,22 @@ contract P12xZetachainOmniBadge is
     }
 
     function initialize(
+        uint256 _voteStartTimeAndDurationDays,
         address _systemContractAddress,
         string memory _tokenUri,
-        address _signerAddress,
-        address initialOwner
+        address _initialOwner,
+        uint64 _payQuantity
     ) public initializer {
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
         $._tokenURI = _tokenUri;
-        $._signer = _signerAddress;
+        $.payQuantity = _payQuantity;
         $.systemContract = SystemContract(_systemContractAddress);
+        $.voteStartTimeAndDurationDays = _voteStartTimeAndDurationDays;
 
         __ERC721_init("P12 x Zetachain OmniBadge", "P12 x Zetachain OmniBadge");
         __EIP712_init("P12 x Zetachain OmniBadge", "V1.0.0");
-        __Ownable_init(initialOwner);
+        __Ownable_init(_initialOwner);
         __ERC721Pausable_init();
         __UUPSUpgradeable_init();
     }
@@ -98,18 +129,32 @@ contract P12xZetachainOmniBadge is
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
         $._tokenURI = newTokenUri;
+
         emit SetTokenUri(newTokenUri);
     }
 
     /**
-     * @dev Update signer(only owner)
-     * @param newSigner New signer adress
+     * @dev Update user pay quantity(only owner)
+     * @param newPayQuantity New pay native token quantity
      */
-    function updateSigner(address newSigner) external onlyOwner {
+    function updatePayQuantity(uint64 newPayQuantity) external onlyOwner {
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
-        $._signer = newSigner;
-        emit SetSigner(newSigner);
+        $.payQuantity = newPayQuantity;
+
+        emit SetPayQuantity(newPayQuantity);
+    }
+
+    /**
+     * @dev Update vote start time and vote duration days(only owner)
+     * @param newVoteStartTimeAndDurationDays New vote start time and duration days
+     */
+    function updateVoteStartTimeAndDurationDays(uint256 newVoteStartTimeAndDurationDays) external onlyOwner {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        $.voteStartTimeAndDurationDays = newVoteStartTimeAndDurationDays;
+
+        emit SetVoteStartTimeAndDurationDays(newVoteStartTimeAndDurationDays);
     }
 
     /**
@@ -120,30 +165,136 @@ contract P12xZetachainOmniBadge is
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
         $.systemContract = SystemContract(newSystemContract);
+
         emit SetSystemContract(newSystemContract);
     }
 
-    /**
-     * @dev Whitelist mint
-     * @param deadline Transaction duration
-     * @param r R short-signature fields
-     * @param vs VS short-signature fields
-     */
-    function whitelistMint(uint256 deadline, bytes32 r, bytes32 vs) external onlyCanMintOnce whenNotPaused {
+    function openCollectionStarts() external onlyOwner {
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
-        if (deadline < block.timestamp) revert MintExpired();
+        $.collectionStarts = !$.collectionStarts;
 
-        bytes32 digest = _hashTypedDataV4(keccak256(abi.encode(WHITELIST_MINT, msg.sender, deadline)));
+        emit CollectionStartsStatus($.collectionStarts);
+    }
 
-        (address recovered, , ) = ECDSA.tryRecover(digest, r, vs);
-        if (recovered != $._signer) revert InvalidSignature();
+    /**
+     * @dev User pays ZETA mint NFT
+     */
+    function openMint() external payable whenNotPaused onlyCanMintOnceAndVerifyPayment {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
-        $.userReceive.setTo(uint256(uint160(msg.sender)), true);
+        $.userReceiveNFT.setTo(uint256(uint160(msg.sender)), true);
+        $.userReceiveCost.setTo(uint256(uint160(msg.sender)), true);
 
         _mint(msg.sender);
 
-        emit UserHasReceived($._nextTokenId, msg.sender);
+        $.userPaysFees[msg.sender] = msg.value;
+
+        emit UserHasReceivedNFT($._nextTokenId, msg.sender);
+    }
+
+    /**
+     * @dev User receive the ZETA paid by mint cost
+     */
+    function userReceiveMintCost() external whenNotPaused onlyCanOpenAndReceivedOnce {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        $.userReceiveCost.setTo(uint256(uint160(msg.sender)), false);
+
+        uint256 userMintFee = $.userPaysFees[msg.sender];
+        $.userPaysFees[msg.sender] -= userMintFee;
+
+        _callSendZETA(msg.sender, userMintFee);
+
+        emit UserHasReceivedCost(msg.sender);
+    }
+
+    /**
+     * @dev Users vote for their favorite games
+     * @param gameId User favorite game id
+     */
+    function userFavoriteGameVote(uint256 gameId) external whenNotPaused onlyDaysUpdateAndMintBehavior{
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        $.userVotingStatus[msg.sender][getVoteDays()] = gameId;
+
+        emit UserVoteForGameID(msg.sender, block.timestamp, gameId);
+    }
+
+    /**
+     * @dev Check whether the user has received it NFT
+     * @param userAddress Check user address
+     * @return true/false
+     */
+    function getUserReceiveNFT(address userAddress) public view returns (bool) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return BitMaps.get($.userReceiveNFT, uint256(uint160(userAddress)));
+    }
+
+    /**
+     * @dev Check whether the user has received it token
+     * @param userAddress Check user address
+     * @return true/false
+     */
+    function getUserReceiveCost(address userAddress) public view returns (bool) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return BitMaps.get($.userReceiveCost, uint256(uint160(userAddress)));
+    }
+
+    /**
+     * @dev Check user mint NFT cost
+     * @return user mint pay quantity
+     */
+    function getPayQuantity() public view returns (uint256) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return $.payQuantity;
+    }
+
+    /**
+     * @dev Check current voting maintenance days
+     * @return Voting maintenance days
+     */
+    function getVoteDays() public view returns (uint256) {
+        // Calculate the number of days that have passed
+        uint256 countDays = (block.timestamp - getVotingStartTime()) / 1 days;
+
+        //Take the smaller value between the number of days in the past
+        //and the number of days voting lasts to prevent users from casting invalid votes.
+        return countDays < getVotingDurationDays() ? countDays : getVotingDurationDays();
+    }
+
+    /**
+     * @dev Check if the current user can vote
+     * @param userAddress User address to be queried
+     * @return true/false
+     */
+    function getUserVoteEligibility(address userAddress, uint256 day) public view returns (bool) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return $.userVotingStatus[userAddress][day] > 0;
+    }
+
+    /**
+     * @dev Check voting start time
+     * @return Voting start time
+     */
+    function getVotingStartTime() public view returns (uint128) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return uint128($.voteStartTimeAndDurationDays >> 64);
+    }
+
+    /**
+     * @dev Check voting duration days
+     * @return Voting duration days
+     */
+    function getVotingDurationDays() public view returns (uint64) {
+        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
+
+        return uint64($.voteStartTimeAndDurationDays & 0xFFFFFFFFFFFFFFFF);
     }
 
     // The following functions are overrides required by Solidity.
@@ -158,20 +309,7 @@ contract P12xZetachainOmniBadge is
         return $._tokenURI;
     }
 
-    /**
-     * @dev Check whether the user has received it
-     * @param userAddress Check user address
-     * @return true/false
-     */
-    function getUserReceive(address userAddress) public view returns (bool) {
-        P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
-
-        return BitMaps.get($.userReceive, uint256(uint160(userAddress)));
-    }
-
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-    // The following functions are overrides required by Solidity.
 
     function onCrossChainCall(
         zContext calldata context,
@@ -192,6 +330,14 @@ contract P12xZetachainOmniBadge is
         P12xZetachainOmniBadgeStorage storage $ = _getP12xZetachainOmniBadgeStorage();
 
         _safeMint(to, ++$._nextTokenId);
+    }
+
+    function _callSendZETA(address userAddress, uint256 amount) private {
+        if (amount > address(this).balance) revert InsufficientZETABalance();
+
+        (bool sent, ) = payable(userAddress).call{value: amount}("");
+
+        if (!sent) revert ZETATransferFailed();
     }
 
     function _update(
