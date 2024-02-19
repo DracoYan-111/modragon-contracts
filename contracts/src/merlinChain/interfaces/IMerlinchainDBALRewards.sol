d58bad401d3d8a99215197804f891cc112ad85a9
P12_Draco <draco@p12.dev>
2024-02-19 13:32:46 +0800

    👷Contract writing completed

--- contracts/src/merlinChain/interfaces/IMerlinchainDBALRewards.sol
+++ contracts/src/merlinChain/interfaces/IMerlinchainDBALRewards.sol
@@ -13,7 +13,8 @@ contract IMerlinchainDBALRewardsDef {
     event UserHasReceivedDBAL();
     event UserHasReceivedRefund();
     event SetTokenAddress(uint256 number, address tokenAddress);
-    event UserMint(uint256 quantity, uint256 totalCost, address tokenAddress);
+    event SetTokenQuantityCharged(uint256 number, uint256 quantityCharged);
+    event UserMint(uint256 quantity, uint256 totalCost, address tokenAddrss);
     event SetMerkleRootInformation(uint256 functionName, bytes32 merkleRoot);
 }
 
