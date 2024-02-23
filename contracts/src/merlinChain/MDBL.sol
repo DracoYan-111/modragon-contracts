289714d044d178bf29d58266f0f311a27d2b9c3f
P12_Draco <draco@p12.dev>
2024-02-23 13:36:20 +0800

    🔄Update MDBL token contract

--- contracts/src/merlinChain/MDBL.sol
+++ contracts/src/merlinChain/MDBL.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable2Step.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MDBL is ERC20, ERC20Burnable, Ownable2Step {
+contract MDBL is ERC20, ERC20Burnable {
     constructor(
-        address initialOwner,
         address fairLaunchTreasury,
         address gameOutputTreasury,
         address initialLiquidityTreasury
-    ) ERC20("MDBL", "MDBL") Ownable(initialOwner) {
+    ) ERC20("MDBL", "MDBL") {
         uint256 total = 2100000000 * 1 ether;
         uint256 fairLaunch = (total * 69) / 100;
         uint256 gameOutput = (total * 30) / 100;
