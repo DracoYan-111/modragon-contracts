8ffe8a2181dc030ceaac20320c9fe540a2e62217
P12_Draco <draco@p12.dev>
2024-03-08 14:02:29 +0800

    🔄Add vest end time check

--- contracts/src/fjordFork/LiquidityBootstrapPoolNew.sol
+++ contracts/src/fjordFork/LiquidityBootstrapPoolNew.sol
@@ -674,8 +674,8 @@ contract LiquidityBootstrapPool is Pausable, Clone, ReentrancyGuard {
         address recipient,
         bool referred
     ) external virtual recipientIsSender(recipient) returns (uint256 shares) {
-        if (!closed) revert RedeemingDisallowed();
-        
+        if (!closed && block.timestamp < vestEnd()) revert RedeemingDisallowed();
+
         shares = purchasedShares[msg.sender];
 
         delete purchasedShares[msg.sender];
