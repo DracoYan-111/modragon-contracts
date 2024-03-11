cfeaa2ee1c75af9f8a69846241f5c6312796b85c
P12_Draco <draco@p12.dev>
2024-03-11 14:08:17 +0800

    🔄Update pool interact

--- scripts/fjordFork/LiquidityBootstrapPoolInteract.ts
+++ scripts/fjordFork/LiquidityBootstrapPoolInteract.ts
@@ -10,46 +10,52 @@ async function main() {
   const liquidityBootstrapPool = await hre.ethers.getContractAt(
     "LiquidityBootstrapPool",
     address,
+    
   ); // Specify here your contract name
 
-  const MDBLPrice = await liquidityBootstrapPool.previewAssetsIn(
-    ethers.parseEther("1"),
-  );
-  console.log(MDBLPrice);
+  console.log(await liquidityBootstrapPool.previewSharesIn(100000));
+  console.log(await liquidityBootstrapPool.previewSharesOut(100000));
 
-  console.log("================= Buy =================");
+  console.log(await liquidityBootstrapPool.reservesAndWeights());
 
-  const buyTx = await liquidityBootstrapPool.swapExactAssetsForShares(
-    MDBLPrice,
-    0,
-    "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
-  );
-  console.log("The transaction hash is: " + `${GREEN}${buyTx.hash}${RESET}\n`);
-  console.log("Waiting until the transaction is confirmed...\n");
-  const buyReceipt = await buyTx.wait();
-  console.log(
-    "The transaction returned the following transaction receipt:\n",
-    buyReceipt,
-  );
-  console.log("================= Sell =================");
+//   const MDBLPrice = await liquidityBootstrapPool.previewAssetsIn(
+//     ethers.parseEther("1"),
+//   );
+//   console.log(MDBLPrice);
 
-  const userShares = await liquidityBootstrapPool.purchasedShares(
-    "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
-  );
-  console.log(userShares);
+//   console.log("================= Buy =================");
 
-  const sellTx = await liquidityBootstrapPool.swapExactSharesForAssets(
-    userShares,
-    0,
-    "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
-  );
-  console.log("The transaction hash is: " + `${GREEN}${sellTx.hash}${RESET}\n`);
-  console.log("Waiting until the transaction is confirmed...\n");
-  const sellReceipt = await sellTx.wait();
-  console.log(
-    "The transaction returned the following transaction receipt:\n",
-    sellReceipt,
-  );
+//   const buyTx = await liquidityBootstrapPool.swapExactAssetsForShares(
+//     MDBLPrice,
+//     0,
+//     "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
+//   );
+//   console.log("The transaction hash is: " + `${GREEN}${buyTx.hash}${RESET}\n`);
+//   console.log("Waiting until the transaction is confirmed...\n");
+//   const buyReceipt = await buyTx.wait();
+//   console.log(
+//     "The transaction returned the following transaction receipt:\n",
+//     buyReceipt,
+//   );
+//   console.log("================= Sell =================");
+
+//   const userShares = await liquidityBootstrapPool.purchasedShares(
+//     "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
+//   );
+//   console.log(userShares);
+
+//   const sellTx = await liquidityBootstrapPool.swapExactSharesForAssets(
+//     userShares,
+//     0,
+//     "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
+//   );
+//   console.log("The transaction hash is: " + `${GREEN}${sellTx.hash}${RESET}\n`);
+//   console.log("Waiting until the transaction is confirmed...\n");
+//   const sellReceipt = await sellTx.wait();
+//   console.log(
+//     "The transaction returned the following transaction receipt:\n",
+//     sellReceipt,
+//   );
 }
 
 main().catch((error) => {
