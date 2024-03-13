1fafd1b9664d97076324f30457fa61b3889ec8c1
P12_Draco <draco@p12.dev>
2024-03-13 16:53:34 +0800

    🔮Improve calling script

--- scripts/fjordFork/LiquidityBootstrapPoolFactoryInteract.ts
+++ scripts/fjordFork/LiquidityBootstrapPoolFactoryInteract.ts
@@ -1,4 +1,5 @@
 import hre from "hardhat";
+import { utils } from "ethers-v5";
 import { ethers } from "hardhat";
 import { PoolSettingsStruct } from "../../typechain-types/contracts/src/fjordFork/LiquidityBootstrapPoolFactory";
 
@@ -14,22 +15,21 @@ async function main() {
   );
 
   console.log("================= Create =================");
-
   const pool: PoolSettingsStruct = {
     asset: "0x3c6585b5DCA5FDb13D47762E0E1F6A89f21d47F4",
     share: "0x916Ea155AE62f7EC989506e0e257959BFe006c07",
     creator: "0x3e8B6e286f78B13C35E11d567935c3aFEECb9003",
     virtualAssets: 0,
-    virtualShares: Date.parse(new Date().toString()) / 1000 + 86400 * 5,
+    virtualShares: 0,
     maxSharePrice: "309485009821345068724781055",
     maxSharesOut: "309485009821345068724781055",
     maxAssetsIn: "309485009821345068724781055",
     weightStart: ethers.parseEther("0.05"),
     weightEnd: ethers.parseEther("0.5"),
-    saleStart: Date.parse(new Date().toString()) / 1000 + 2000,
-    saleEnd: Date.parse(new Date().toString()) / 1000 + 86400 * 5,
-    vestCliff: 0,
-    vestEnd: 0,
+    saleStart: Date.parse(new Date().toString()) / 1000,
+    saleEnd: Date.parse(new Date().toString()) / 1000 + 86400 * 2,
+    vestCliff: Date.parse(new Date().toString()) / 1000 + 86400 * 2,
+    vestEnd: Date.parse(new Date().toString()) / 1000 + 86400 * 2 + 31000,
     sellingAllowed: true,
     whitelistMerkleRoot:
       "0x0000000000000000000000000000000000000000000000000000000000000000",
@@ -40,9 +40,9 @@ async function main() {
       pool,
       ethers.parseEther("2100000000"),
       ethers.parseEther("3"),
-      "0x5683725624587456346234573542634567300000000000000000000000000000",
+      utils.solidityKeccak256(["string"], [pool]),
     );
-    
+
   console.log(
     "The transaction hash is: " +
       `${GREEN}https://testnet-scan.merlinchain.io/tx/${createPoolTx.hash}${RESET}\n`,
@@ -51,7 +51,7 @@ async function main() {
   const buyReceipt = await createPoolTx.wait();
   console.log(
     "The transaction returned the following transaction receipt:\n",
-    buyReceipt,
+    buyReceipt?.blockNumber,
   );
 }
 
