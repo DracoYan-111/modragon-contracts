a4e5156c4e82f6a80e140f5c5950ef625f3a972a
P12_Draco <draco@p12.dev>
2024-03-26 11:25:53 +0800

    👷Test use scripts

--- scripts/generate_merkle_root.ts
+++ scripts/generate_merkle_root.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
 // ============== Building a Tree ==============
-const values = require("./airdrop.json");
+const values = require("./merlinRewardMerkleData.json");
 
 const tree = StandardMerkleTree.of(values, ["uint256", "address", "uint256[]", "uint256[]"]);
 
@@ -19,7 +19,7 @@ require("fs").writeFileSync("./tree.json", JSON.stringify(treeDataWithRoot));
 // ============== Obtaining a Proof ==============
 const trees = StandardMerkleTree.load(JSON.parse(fs.readFileSync("./tree.json", "utf8")).tree);
 
-const checkUserAddress = '0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db'
+const checkUserAddress = '0x171Eb9A1799Ec51cD75AA6652E3D8aBb26B67848'
 for (const [i, v] of trees.entries()) {
   if (v[1] === checkUserAddress) {
     const proof = trees.getProof(i);
