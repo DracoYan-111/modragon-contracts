7e2c5882a3058214e9bddadc00a19b98824828d5
P12_Draco <draco@p12.dev>
2024-04-01 18:46:38 +0800

    ✨Update folder name

--- deploy/moboxGovernForge/001_deploy_moboxGovernForgeV2.ts
+++ deploy/moboxGovernForge/001_deploy_moboxGovernForgeV2.ts
@@ -1,7 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { keccak256, stringToBytes, encodeFunctionData } from 'viem';
-import * as contractData from '../artifacts/contracts/src/MoboxGovernForge/MoboxGovernForgeV2.sol/MoboxGovernForgeV2.json';
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
