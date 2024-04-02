cd48d98208c31553be92a6025f16f10b229abad2
P12_Draco <draco@p12.dev>
2024-04-02 21:01:10 +0800

    Add eMDBL contract interfaces

---
+++ contracts/src/mdblLBP/mdblStake/interfaces/IeMDBL.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+interface IeMDBLDef {
+    error MulWadFailed();
+    error InvalidAmount();
+    error InvalidDuration();
+    error RedemptionFinish();
+    error RedemptionNotEnded();
+    error NoMintLimitAvailable();
+    error NotEnoughAvailableAmount();
+
+    event UserSwapEMDBL(address user, uint256 amount);
+    event PermitMintToken(address user, uint256 amount);
+    event RedemptionStarted(address user, uint256 index);
+    event RedemptionCancelled(address user, uint256 index);
+    event RedemptionCompleted(address user, uint256 amount);
+}
+
+interface IeMDBL is IeMDBLDef {}
