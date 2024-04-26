db3a5e829304504ce0b0720abcae26c968642f14
P12_Draco <draco@p12.dev>
2024-04-26 14:05:00 +0800

    💎Add deploy script

--- deploy/merlinChain/001_deploy_rewardDistribution.ts
+++ deploy/merlinChain/001_deploy_rewardDistribution.ts
@@ -10,40 +10,44 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
 
-  console.log(deployer)
+  console.log(deployer);
   /**
-        address _initialOwner,
-        IERC721 _blueboxAddr,
-        IERC721 _musicboxAddr,
-        bytes32 _receiveRoot
+  address _initialOwner,
+  IERC721 _blueboxAddr,
+  IERC721 _musicboxAddr,
+  address _blueboxOwner,
+  address _musicboxOwner,
+  bytes32 _receiveRoot
    */
   const args = [
     deployer,
-    "0x0000000000000000000000000000000000000000",
-    "0x0000000000000000000000000000000000000000",
-    "0x0000000000000000000000000000000000000000000000000000000000000000"
+    "0xd32E14CC9aa5413622fD00b28e50aDFbf25c7EF8",
+    "0x1EB48c61402502c655e6a59b30E9f1C93121F701",
+    "0x6F003a7A0f8a2D1b6154e77960AEd19dee103328",
+    "0x6F003a7A0f8a2D1b6154e77960AEd19dee103328",
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
   ];
 
-  let contract = await deploy('RewardDistribution', {
+  let contract = await deploy("RewardDistribution", {
     from: deployer,
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
     proxy: {
-      checkProxyAdmin:false,
-      proxyContract: 'ERC1967Proxy',
-      proxyArgs: ['{implementation}', '{data}'],
+      checkProxyAdmin: false,
+      proxyContract: "ERC1967Proxy",
+      proxyArgs: ["{implementation}", "{data}"],
       upgradeFunction: {
         methodName: "upgradeToAndCall",
-        upgradeArgs: ['{implementation}', '{data}']
+        upgradeArgs: ["{implementation}", "{data}"],
       },
       execute: {
         init: {
-          methodName: 'initialize',
+          methodName: "initialize",
           args: args,
         },
       },
     },
-    deterministicDeployment: keccak256(stringToBytes('RewardDistribution_PROD')),
+    deterministicDeployment: keccak256(stringToBytes("RewardDistribution")),
   });
 
   // TODO If you want to cancel "verification", please enable comments
