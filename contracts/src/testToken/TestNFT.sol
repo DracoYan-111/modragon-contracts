f288627200705b4925bb7095b33df383a7bfc5ea
P12_Draco <draco@p12.dev>
2024-05-16 15:00:54 +0800

    🔄Update TestNFT contract

--- contracts/src/testToken/TestNFT.sol
+++ contracts/src/testToken/TestNFT.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestNFT is ERC721, Ownable {
+contract TestNFT is ERC721, ERC721Enumerable, Ownable {
     uint256 private _nextTokenId;
 
     constructor(address initialOwner, string memory name) ERC721(name, name) Ownable(initialOwner) {}
@@ -22,5 +24,23 @@ contract TestNFT is ERC721, Ownable {
                 ++i;
             }
         }
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
