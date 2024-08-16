5d778c6220eacabb0b5d62cefdbd6e0289f558e1
P12_Draco <draco@p12.dev>
2024-08-16 17:38:30 +0800

    ✅RankTokenDistribution deploy script

---
+++ deploy/merlinChain/001_deploy_rankTokenDistribution.ts
@@ -0,0 +1,51 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { keccak256, stringToBytes, encodeFunctionData } from "viem";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deploy } = hre.deployments;
+  const { deployer } = await hre.getNamedAccounts();
+
+  /**
+        address initialOwner,
+   */
+  const args = [deployer];
+
+  let contract = await deploy("RankTokenDistribution", {
+    from: deployer,
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    proxy: {
+      checkProxyAdmin: false,
+      proxyContract: "ERC1967Proxy",
+      proxyArgs: ["{implementation}", "{data}"],
+      upgradeFunction: {
+        methodName: "upgradeToAndCall",
+        upgradeArgs: ["{implementation}", "{data}"],
+      },
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: args,
+        },
+      },
+    },
+    deterministicDeployment: keccak256(
+      stringToBytes("RankTokenDistribution_PRD"),
+    ),
+  });
+  // // TODO If you want to cancel "verification", please enable comments
+  // console.log(
+  //     "Waiting 30 seconds before beginning the contract verification to allow the block explorer to index the contract...\n",
+  //   );
+
+  // await delay(30000); // Wait for 30 seconds before verifying the contract
+
+  // await hre.run("verify:verify", {
+  //     address: contract.address,
+  //     constructorArguments: args,
+  // });
+};
+export default func;
+func.id = "001_deploy_rankTokenDistribution"; // id required to prevent reexecution
+func.tags = ["RankTokenDistribution"];
